3d4a2beb46da0c434b7b03f880a17aa4f57823c9
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-09-26 12:44:33 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Task 1 - Print Text
+print("Welcome to COM411!")
+
+# This is a comment
+
+# Print an empty string
+print()
+
+''' Displays "some text" to the standard output. 
+It is preferable to place comments before the code. '''
+print("some text")
+
+# Commit to remote repository
+
