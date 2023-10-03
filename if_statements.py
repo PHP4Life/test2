dc1208dbf8834f4d5bcde5e9f6ddba23752993e9
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-03 11:34:56 +0100

    Added code to demonstate the use of an if...elif...else statement

--- if_statements.py
+++ if_statements.py
@@ -2,17 +2,33 @@
 # Task 1 - Print book type
 # book = input("Enter the type of book: ")
 #
-# if book == "adventure":
+# if book == "adventure": # Checks if the variable $book is equal to "adventure"
 #     print("I like adventure books!")
 #
 # print("Finished reading book!")
 
+
 # Task 2 - If & Else statements
-activity = input("Please enter the activity to be performed:")
+# activity = input("Please enter the activity to be performed:")
+#
+# if activity == "calculate": # Checks if the variable $activity is equal to "calculate"
+#     print("Performing calculation")
+# else:
+#     print("Performing activity")
+#
+# print("Activity completed")
 
-if activity == "calculate":
-    print("Performing calculation")
+
+# Task 3 - If...Elif...Else statement
+direction = input("Enter the direction the robot should move (i.e. up, down, left or right):")
+
+if direction == "up":
+    print("I am moving in an upward direction!")
+elif direction == "down":
+    print("I am moving in a downward direction!")
+elif direction == "left":
+    print("I am moving left")
+elif direction == "right":
+    print("I am moving right")
 else:
-    print("Performing activity")
-
-print("Activity completed")
+    print("I am stationary")
