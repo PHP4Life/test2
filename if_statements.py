4bc5fe58037259ec52b23f7d45942581bfe18f55
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-03 11:56:46 +0100

    added code to demonstrate the use of comparison operators

--- if_statements.py
+++ if_statements.py
@@ -1,43 +1,57 @@
 
-# Task 1 - Print book type
-book = input("Enter the type of book: ")
+# # Task 1 - Print book type
+# book = input("Enter the type of book: ")
+#
+# if book == "adventure": # Checks if the variable $book is equal to "adventure"
+#     print("I like adventure books!")
+#
+# print("Finished reading book!")
+#
+#
+# # Task 2 - If & Else statements
+# activity = input("Please enter the activity to be performed:")
+#
+# if activity == "calculate": # Checks if the variable $activity is equal to "calculate"
+#     print("Performing calculation")
+# else:
+#     print("Performing activity")
+#
+# print("Activity completed")
+#
+#
+# # Task 3 - If...Elif...Else statement
+# direction = input("Enter the direction the robot should move (i.e. up, down, left or right):")
+#
+# if direction == "up":
+#     print("I am moving in an upward direction!")
+# elif direction == "down":
+#     print("I am moving in a downward direction!")
+# elif direction == "left":
+#     print("I am moving left")
+# elif direction == "right":
+#     print("I am moving right")
+# else:
+#     print("I am stationary")
+#
+#
+# # Task 4 - Modulo Operator
+# number = input("Enter a number to determine whether it is odd or even:")
+#
+# if int(number) % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+#
 
-if book == "adventure": # Checks if the variable $book is equal to "adventure"
-    print("I like adventure books!")
+# Task 5 - Comparison Operators
+number1 = input("Enter your first number: ")
+number2 = input("Enter your second number: ")
 
-print("Finished reading book!")
-
-
-# Task 2 - If & Else statements
-activity = input("Please enter the activity to be performed:")
-
-if activity == "calculate": # Checks if the variable $activity is equal to "calculate"
-    print("Performing calculation")
+if int(number1) < int(number2):
+    print("The first number is the smallest")
+elif int(number1) > int(number2):
+    print("The second number is the smallest")
 else:
-    print("Performing activity")
+    print("Both are equal")
 
-print("Activity completed")
-
-
-# Task 3 - If...Elif...Else statement
-direction = input("Enter the direction the robot should move (i.e. up, down, left or right):")
-
-if direction == "up":
-    print("I am moving in an upward direction!")
-elif direction == "down":
-    print("I am moving in a downward direction!")
-elif direction == "left":
-    print("I am moving left")
-elif direction == "right":
-    print("I am moving right")
-else:
-    print("I am stationary")
-
-
-# Task 4 - Modulo Operator
-number = input("Enter a number to determine whether it is odd or even:")
-
-if int(number) % 2 == 0:
-    print("even")
-else:
-    print("odd")
+# Task 6 -
