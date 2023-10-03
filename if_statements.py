0290b66d797430c53eb986aadb671912c19a1c93
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-03 11:50:09 +0100

    added code to demonstate the use of a modulus operator

--- if_statements.py
+++ if_statements.py
@@ -1,22 +1,22 @@
 
 # Task 1 - Print book type
-# book = input("Enter the type of book: ")
-#
-# if book == "adventure": # Checks if the variable $book is equal to "adventure"
-#     print("I like adventure books!")
-#
-# print("Finished reading book!")
+book = input("Enter the type of book: ")
+
+if book == "adventure": # Checks if the variable $book is equal to "adventure"
+    print("I like adventure books!")
+
+print("Finished reading book!")
 
 
 # Task 2 - If & Else statements
-# activity = input("Please enter the activity to be performed:")
-#
-# if activity == "calculate": # Checks if the variable $activity is equal to "calculate"
-#     print("Performing calculation")
-# else:
-#     print("Performing activity")
-#
-# print("Activity completed")
+activity = input("Please enter the activity to be performed:")
+
+if activity == "calculate": # Checks if the variable $activity is equal to "calculate"
+    print("Performing calculation")
+else:
+    print("Performing activity")
+
+print("Activity completed")
 
 
 # Task 3 - If...Elif...Else statement
@@ -32,3 +32,12 @@ elif direction == "right":
     print("I am moving right")
 else:
     print("I am stationary")
+
+
+# Task 4 - Modulo Operator
+number = input("Enter a number to determine whether it is odd or even:")
+
+if int(number) % 2 == 0:
+    print("even")
+else:
+    print("odd")
