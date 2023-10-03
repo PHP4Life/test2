cdb2404ca0af8a8d2351c3500e2b4e32ed24fb2f
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-03 11:14:49 +0100

    Added code to demonstrate the use of an if statement

---
+++ if_statements.py
@@ -0,0 +1,10 @@
+
+# Task 1 - Print book type
+book = input("Enter the type of book: ")
+
+if book == "adventure":
+    print("I like adventure books!")
+
+print("Finished reading book!")
+
+# Task 2 -
