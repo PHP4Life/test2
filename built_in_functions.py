74c6b9d8ba13b99cade786fea52ce623f61fc2f0
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-17 11:27:00 +0100

    Added code to demonstrate the use of built-in functions: abs, range, chr.

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,7 +1,6 @@
 # Functions
 
-# Task 1 - ASCII Converter
-
+# Task 1 - Character to ASCII Converter
 def ascii_converter():
     print("Program Started!")
     character_input = input("Please enter a standard character: ")
@@ -14,3 +13,15 @@ def ascii_converter():
 
 
 ascii_converter()
+
+
+# Task 2 - ASCII to Character Converter
+def character_converter():
+    print("Program Started!")
+    ascii_input = input("Please enter an ASCII code: ")
+
+    character = chr(int(ascii_input))
+    print(f"The character represented by the ASCII code {ascii_input} is {character}")
+
+
+character_converter()
