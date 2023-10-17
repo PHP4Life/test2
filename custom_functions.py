8796e3d84b6cdb0c473d2a4e588634ac05df69a6
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-17 11:32:57 +0100

    Added code to demonstrate the use of a user-defined function.

---
+++ custom_functions.py
@@ -0,0 +1,10 @@
+# Custom Functions
+
+# Task 1 - Text adventure game
+
+def listen():
+    sound = input("Enter a word representing a sound: ")
+    print(f"That was a loud {sound}!")
+
+
+listen()
