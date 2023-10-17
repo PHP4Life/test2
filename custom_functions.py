fba965e7a2e2c5b3e411c28836b6ab2a7e3b789a
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-10-17 11:42:43 +0100

    Added indentify function to custom_functions.py

--- custom_functions.py
+++ custom_functions.py
@@ -7,4 +7,13 @@ def listen():
     print(f"That was a loud {sound}!")
 
 
+def identify():
+    identify_object = input("Enter a word representing what they see: ")
+    if identify_object == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine")
+
+
 listen()
+identify()
