69b9c0e6a51ea3284f65eb7148f1456cf56add00
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-11-07 12:01:25 +0000

    Added code to demonstrate how to iterate through a dictionary

--- dictionary_tasks.py
+++ dictionary_tasks.py
@@ -7,9 +7,27 @@ def run():
     print(pattern())
 
 
+def display_keys(data):
+    for key in data.keys():
+        print("The key is "+key)
+
+
+def display_values(data):
+    for value in data.values():
+        print("The value is "+str(value))
+
+
+def display_items(data):
+    for key, value in data.items():
+        print(f"The key is: {key}: {value}")
+
+
 def run_task():
-    pattern()
+    data = pattern()
     run()
+    display_keys(data)
+    display_values(data)
+    display_items(data)
 
 
 run_task()
