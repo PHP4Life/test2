3aec45fe866fb917307e386f5a9c812897c21bd9
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-11-14 09:51:21 +0000

    Added code to display a simple line plot

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
+import matplotlib.pyplot as plt
 
-# Task 1 - Print Text
-print("Welcome to COM411!")
 
-# This is a comment
+# Using Third-Party Libraries
 
-# Print an empty string
-print()
+# Task 1 - Basic line graph
 
-''' Displays "some text" to the standard output. 
-It is preferable to place comments before the code. '''
-print("some text")
+def display_line(x_values, y_values):
+    plt.plot(x_values, y_values)
+    plt.show()
 
-# Commit to remote repository
 
-# Task 2 - Multi line comments
+def run_task():
+    x_values = [1, 2, 3, 4, 5] # Values for the x-axis
+    y_values = [1, 4, 9, 16, 25] # Values for the y-axis
+    display_line(x_values, y_values)
 
-# Display message to the screen
-print("Welcome to COM411!")
-print("In week 1 we will learn...")
+
+run_task()
