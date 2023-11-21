d9cc9bd37a9549ab589e1ac0a79a0bacffbf42a1
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-11-21 11:58:41 +0000

    Updated classes human and robot with instance methods.

--- human.py
+++ human.py
@@ -1,4 +1,3 @@
-
 class Human:
     MAX_ENERGY = 100
 
@@ -13,6 +12,21 @@ class Human:
 
     def __str__(self):
         return f'{self.name} is {self.age} years old.'
+
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        if self.energy + amount > self.MAX_ENERGY:
+            print("Energy is full")
+        else:
+            self.energy += amount
+
+    def move(self, distance):
+        if self.energy == 0:
+            print("unable to move, eat to gain more energy")
+        else:
+            self.energy -= distance
 
     def display(self):
         print(f"I am {self.name}")
