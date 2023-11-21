c5108f1bb8305400e25e2177fa5be03de74a4b87
EDWARD KIRR <2kirre82@solent.ac.uk>
2023-11-21 11:44:00 +0000

    Updated classes human and robot with methods to display objects.

--- human.py
+++ human.py
@@ -8,5 +8,11 @@ class Human:
         self.age = 0
         self.energy = energy
 
+    def __repr__(self):
+        return f'(name={self.name}, age={self.age})'
+
+    def __str__(self):
+        return f'{self.name} is {self.age} years old.'
+
     def display(self):
         print(f"I am {self.name}")
--- main.py
+++ main.py
@@ -5,5 +5,10 @@ from robot import Robot
 if __name__ == "__main__":
     human1 = Human()
     human1.display()
+    print(human1)
+    print(repr(human1))
+
     robot1 = Robot()
     robot1.display()
+    print(robot1)
+    print(repr(robot1))
