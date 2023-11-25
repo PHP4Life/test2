1e1d41bb62fb5744ee5d07679697338f9e58ae7a
PHP4Life <PHP4Life@github.com>
2023-11-25 19:07:54 +0000

    hello

---
+++ olympics/tui.py
@@ -0,0 +1,11 @@
+# Olympics task
+
+def started(msg=""):
+    print("------------------------------------------------------------------------------------\n")
+    print("Operation started: {msg}... \n")
+
+def run_task():
+    started("hello")   
+
+
+run_task    
